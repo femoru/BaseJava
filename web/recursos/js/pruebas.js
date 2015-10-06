710adbd93e8bb0b372fd87f4e19a5ecfc53902ee
bryan munoz <bmunoz@INGENIERIAA4921.DCSIO.SIO>
2015-10-06 16:52:31 -0500

    modificación a interfaz de pruebas para ensayos con distintos metodos de la libreria JQgrid

--- web/recursos/js/pruebas.js
+++ web/recursos/js/pruebas.js
@@ -1,79 +1,159 @@
 var data = [{
           id : "1",
-          Integer : "2000000",
+          Integer : "10000",
           Number : "562.2",
           Email : "Comfandi@hotmail.com",
           Url : "http://www.google.com"
   }];
   
-  $("#jqGrid").jqGrid({
-                url: 'data.json',
-                //editurl: 'post.json',
+ 			if (!window.console) window.console = {};
+			if (!window.console.log) window.console.log = function () { };
+
+                    $("#jqGrid").jqGrid({
+               // url: 'data.json',
+                //editurl: 'clientArray',
                 datatype: "local",
                 data:data,
+                styleUI : 'Bootstrap',
                 colModel: [
                     {
                         name: 'Integer',
+                        key: true,
                         editable: true,
+                        editrules:{
+                            //custom rules
+                            custom_func: validatePositive,
+                            custom: true,
+                            //required: true
+                        },
+                        editoptions: {
+                            dataEvents: [
+                                 { 
+                                type: 'keydown', 
+                                fn: onEnter
+                                 }
+                            ]},
+                        
                         width: 75
                     },
                     {
                         name: 'Number',
                         width: 100,
-                        editable: true
+                        editable: true,
+                        editrules: {
+                           custom_func: validatePositive,
+                            custom: true
+                        },
+                        editoptions: {
+                            dataEvents: [
+                                 { 
+                                type: 'keydown', 
+                                fn: onEnter
+                                 }
+                            ]},
                     },
                     {
                         name: 'Email',
                         width: 100,
-                        editable: true
+                        editable: true,
+                        editrules: {
+                            email: true,
+                            required: true
+                        }
                     },
                     {
                         name: 'Url',
                         width: 80,
-                        editable: true
+                        editable: true,
+                        editrules: {
+                            url: true,
+                            required: true
+                        }
                     }
                 ],
 				loadonce : true,
-                height: 200,
-                rowNum: 10,
                 onSelectRow: editRow,
-                pager: "#jqGridPager"
-            });
+                width: 780,
+                height: 200,
+                 rowNum : 10,
+                 rowList : [10, 20, 30 ,50 ,100 ,500,1000],
+                toolbarfilter: true,
+                 viewrecords : true,
+                pager: "#jqGridPager",
+                loadComplete: function (data) {
+                var grid = jQuery("#jqGrid"),//creador de filas vacias
+                
+                  pageSize = parseInt(grid.jqGrid("getGridParam", "rowNum")),
+                  emptyRows = pageSize - data.rows.length;
 
-            var lastSelection;
+                if (emptyRows > 0) {
+                  for (var i = 1; i <= emptyRows; i++)
+                      // Send rowId as undefined to force jqGrid to generate random rowId
+                      grid.jqGrid('addRowData', undefined, {});
 
-            function editRow(id) {
-                if (id && id !== lastSelection) {
-                    var grid = $("#jqGrid");
-                    grid.restoreRow(lastSelection);
-
-                    var editParameters = {
-                        keys: true,
-                        successfunc: editSuccessful,
-                        errorfunc: editFailed,
-			restoreAfterError : false
-                    };
-                    grid.jqGrid('editRow',id, editParameters);
-                    lastSelection = id;
+                  // adjust the counts at lower right
+                  grid.jqGrid("setGridParam", {
+                    reccount: grid.jqGrid("getGridParam", "reccount") - emptyRows,
+                    records: grid.jqGrid("getGridParam", "records") - emptyRows
+                  });
+                  grid[0].updatepager();
                 }
-            }
+              }
+        });
 
-            function editSuccessful( data, stat) {
-                    var response = data.responseJSON;
-                    if (response.hasOwnProperty("error")) {
-                            if(response.error.length) {
-                                    return [false,response.error ];
-                            }
-                    }
-                    return [true,"",""];
-            }
+        var lastSelection;
 
-            function editFailed(rowID, response) {
+        function onEnter(e){
+
+            var key = e.charCode || e.keyCode;
+             if (key == 9 || key ==13){
+                var sel_id = $("#jqGrid").getGridParam('selrow');
+               // alert(sel_id);
+                var index = $("#jqGrid").jqGrid('getInd',sel_id); 
+                var cm = $("#jqGrid").jqGrid("getGridParam", "colNames");
+                //alert(cm[index-1]);
+
                 $.jgrid.info_dialog(
-                                $.jgrid.regional["en"].errors.errcap,
-                '<div class="ui-state-error">RowID:sdf</div>', 
-                $.jgrid.regional["en"].edit.bClose,
-                {buttonalign:'right', styleUI : 'Bootstrap'}
+                $.jgrid.regional["es"].errors.errcap,
+                '<div class="ui-state-error">Faltan datos por ingresar</div>', 
+                $.jgrid.regional["es"].edit.bClose,
+                {buttonalign:'right', styleUI : 'Bootstrap', zIndex: 1234}
                 );
-                //alert(response.responseJSON.error);
+             }
+        }
+        function editRow(id) {
+            if (id && id !== lastSelection) {
+                var grid = $("#jqGrid");
+                grid.jqGrid('restoreRow',lastSelection);
+
+                var editParameters = {
+                    keys: true,
+                    successfunc: editSuccessful,
+                    errorfunc: editFailed
+                };
+
+                grid.jqGrid('editRow',id, editParameters);
+                lastSelection = id;
             }
+        }
+
+        function editSuccessful() {
+            //console.log("success");
+        }
+
+        function editFailed() {
+            //console.log("fail");
+            $.jgrid.info_dialog(
+            $.jgrid.regional["es"].errors.errcap,
+            '<div class="ui-state-error">es un campo númerico</div>', 
+            $.jgrid.regional["es"].edit.bClose,
+            {buttonalign:'right', styleUI : 'Bootstrap', zIndex: 1234}
+                 )
+        }
+
+        function validatePositive(value, column) {
+            if ( isNaN(value) && value < 0)
+                return [false, "Please enter a positive value or correct value"];
+            else
+                return [true, ""];
+        }                
